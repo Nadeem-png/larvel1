f861593f2452e8e7610c87721c95b223f4fd9237
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-07-01 12:15:51 +0500

    Update layout.blade.php
    Nav brand

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -40,13 +40,13 @@
 <nav class="navbar nav1   navbar-expand-lg"style="height: 70px;background-color:#2f3640 ">
 <div class="w-25">
         <a href="/" class="navbar-brand">
-            <img src="{{asset('image/project2.jpg.png')}}" class="img-fluid w-50 h-50" style="margin-left:185px">
+            <img src="{{asset('img/nadeem.jpeg')}}" class="img-fluid rounded-circle w-25 h-25" style="margin-left:185px">
         </a>
     </div>
     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#mynav"><i class="fas fa-bars text-white"></i></button>
     
     <div class="navbar-collapse   collapse" id="mynav">
-        <ul class="navbar-nav mt-5 ">
+        <ul class="navbar-nav mt-3 ">
         <li class="nav-item" @yield('nava')>
                     <a href="/" class="nav-link a1  ml-5 font-weight-bold">Home</a>
                 </li>
