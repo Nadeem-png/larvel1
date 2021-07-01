8e120535a5010f67a6ae64c0e03f29289bd23311
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-07-01 18:47:08 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,7 +102,7 @@ footer{
 <div class="container container-sm">
 <h3 class="text-center py-5 text-white">PORTFOLIO</h3>
 
-<ul class="nav nav-pills text-center mb-3" id="pills-tab" role="tablist">
+<ul class="nav nav-pills text-center d-flex justify-content-center align-items-center mb-3" id="pills-tab" role="tablist">
   <li class="nav-item" role="presentation">
     <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">All</a>
   </li>
@@ -112,11 +112,15 @@ footer{
   <li class="nav-item" role="presentation">
     <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">React.js</a>
   </li>
+<li class="nav-item" role="presentation">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills" role="tab" aria-controls="pills-contact" aria-selected="false">Landing pages</a>
+  </li>
 </ul>
 <div class="tab-content" id="pills-tabContent">
   <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
   <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
   <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+<div class="tab-pane fade" id="pills" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
 </div>
 </div>
 </div>
