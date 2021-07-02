21ef671915c639ded5061ae7b4c026d50584e448
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-07-02 14:52:30 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,15 +21,63 @@ footer{
 .card1{
     border-radius:10px;
 }
+header {
+  position: relative;
+  background-color: black;
+  height: 75vh;
+  min-height: 25rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+header video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: 0;
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+}
+
+header .container {
+  position: relative;
+  z-index: 2;
+}
+
+header .overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  opacity: 0.5;
+  z-index: 1;
+}
+
 
  </style>
- <video autoplay="autoplay" muted loop id="myvideo" height="auto" width="100%">
- <source src="img/network.mp4" type="video/mp4" >
-<div class="container container-sm mt-5 text-center text-white">
-<h1>Hey there,I am</h1>
-<h2>Nadeem KHAN full stack web developer</h2>
-</div>
- </video>
+ 
+<header>
+  <div class="overlay"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="img/network.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1 class="display-3">Video Header</h1>
+        <p class="lead mb-0">With HTML5 Video and Bootstrap 4</p>
+      </div>
+    </div>
+  </div>
+</header>
  <div class="container container-sm mt-5">
  <u ><h3 class="text-center mb-5" >About</h3></u>
 <div data-aos="fade-up"
