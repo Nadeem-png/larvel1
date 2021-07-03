740f797be74bbb7a7a97506d2f090de8f11917f2
Nadeem-png <59850010+Nadeem-png@users.noreply.github.com>
2021-07-03 09:19:21 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,69 @@
 footer{
     background-color:#191a1c;
 }
-.pricing{
-    background-color:#2f3640;
+
+section.pricing {
+background-color:#2f3640;
     width:100%;
     height:auto;
+  }
+
+.pricing .card {
+  border: none;
+  border-radius: 1rem;
+  transition: all 0.2s;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.pricing hr {
+  margin: 1.5rem 0;
+}
+
+.pricing .card-title {
+  margin: 0.5rem 0;
+  font-size: 0.9rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+}
+
+.pricing .card-price {
+  font-size: 3rem;
+  margin: 0;
+}
+
+.pricing .card-price .period {
+  font-size: 0.8rem;
+}
+
+.pricing ul li {
+  margin-bottom: 1rem;
+}
+
+.pricing .text-muted {
+  opacity: 0.7;
+}
+
+.pricing .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  opacity: 0.7;
+  transition: all 0.2s;
+}
+
+/* Hover Effects on Card */
+
+@media (min-width: 992px) {
+  .pricing .card:hover {
+    margin-top: -.25rem;
+    margin-bottom: .25rem;
+    box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.3);
+  }
+  .pricing .card:hover .btn {
+    opacity: 1;
+  }
 }
 .portfolio{
     background-color:#2f3640;
@@ -226,73 +285,75 @@ header .overlay {
 
 
 </section>
-<section id="pricing">
-<div class="pricing">
-<div class="container container-sm">
-<h3 class="text-center py-5 text-white">PRICING PER BUILD</h3>
-<div class="row">
-<div class="col-12 col-lg-4 mb-5">
-<div data-aos="fade-up"
-     data-aos-duration="3000">
-<div class="card card1 h-100 text-center">
-  <div class="card-header font-weight-bold ">
-    BASIC
-  </div>
-  <div class="card-body">
-    <h6 class="card-text mb-3">Website Audit</h6>
-    <h6 class="card-text mb-3">Design</h6>
-    <h6 class="card-text mb-4">Development</h6>
-    <a href="#" class="btn btn-outline-dark rounded-pill mb-2">Request Quote</a>
-  </div>
-</div>
-</div>
-</div>
-<div class="col-12 col-lg-4 mb-5">
-<div data-aos="fade-up"
-     data-aos-duration="3000">
-<div class="card card1 h-100 text-center">
-  <div class="card-header font-weight-bold bg-primary ">
-    STANDARD
-  </div>
-  <div class="card-body">
-    <h6 class="card-text mb-3">Website Audit</h6>
-    <h6 class="card-text mb-3">Design</h6>
-    <h6 class="card-text mb-4">Development</h6>
-    <h6 class="card-text mb-4">Backups</h6>
-    <h6 class="card-text mb-4">Analytics</h6>
-    <h6 class="card-text mb-4">Live Chat</h6>
-    <h6 class="card-text mb-4">SEO</h6>
-    <h6 class="card-text mb-4">CMS</h6>
-    <a href="#" class="btn btn-outline-primary rounded-pill mb-2">Request Quote</a>
-  </div>
-</div>
-</div>
-</div>
-<div class="col-12 col-lg-4 mb-5">
-<div data-aos="fade-up"
-     data-aos-duration="3000">
-<div class="card card1 h-100 text-center">
-  <div class="card-header font-weight-bold bg-warning ">
-    PREMIUM
-  </div>
-  <div class="card-body">
-  <h6 class="card-text mb-3">Website Audit</h6>
-    <h6 class="card-text mb-3">Design</h6>
-    <h6 class="card-text mb-4">Development</h6>
-    <h6 class="card-text mb-4">Backups</h6>
-    <h6 class="card-text mb-4">Analytics</h6>
-    <h6 class="card-text mb-4">Live Chat</h6>
-    <h6 class="card-text mb-4">SEO</h6>
-    <h6 class="card-text mb-4">CMS</h6>
-    <a href="#" class="btn btn-outline-warning rounded-pill mb-2">Request Quote</a>
-  </div>
-</div>
-</div>
-</div>
-</div>
-</div>
 
-</div>
+<section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Free Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+          </div>
+        </div>
+      </div>
+      <!-- Plus Tier -->
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+          </div>
+        </div>
+      </div>
+      <!-- Pro Tier -->
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
+            <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+            <hr>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong></li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+            </ul>
+            <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
  <section id="contact">
     <div class="contactbg">
